e580731d65da7064f9171c6bf4e9bd87591aff72
yrsn7vnw7t-svg <yrsn7vnw7t@privaterelay.appleid.com>
2026-08-24 18:21:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Crypto Dashboard",
+    page_icon="📈",
+    layout="wide"
+)
+
+st.title("📈 Mijn Crypto Dashboard")
+st.write("Dashboard is online 🚀")
